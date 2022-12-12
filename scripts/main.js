0d8ac57c1a8857070508a01759a9c44df33771c7
Suncerie <sunceried@gmail.com>
2022-12-11 17:38:00 -0800

    Event Listeners added to actor selector

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,13 @@ const actors = document.querySelectorAll(".actor");
 
 //TODO: Define onClick event handler
 const handler = (event) => {
-    console.log(event);
+    const actorToHighlight = event.target.value;
 
+    //Get all of the <p> for the actor
+    //loop throught each element and assign a background-color of yellow
 }
 
 //TODO: ADD onCLick Listeners to each option
+actors.forEach((actor) => {
+    actor.addEventListener('click',handler);
+});
