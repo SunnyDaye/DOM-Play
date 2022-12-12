8b200744c4ab5915842edcf8c9692a0be7aaa2cb
Suncerie <sunceried@gmail.com>
2022-12-11 17:24:53 -0800

    General styling finished

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const actors = document.querySelectorAll(".actor");
+
+//TODO: Define onClick event handler
+const handler = (event) => {
+    console.log(event);
+
+}
+
+//TODO: ADD onCLick Listeners to each option
